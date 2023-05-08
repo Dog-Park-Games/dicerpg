5ac626d6f1eae28a29f9264968d981d369ae8233
martinwinther <mswinther@icloud.com>
2023-05-08 10:34:35 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("hero").innerHTML = `<div class="character-card">
+<h4 class="name"> Winnie </h4>
+<img class="avatar" src="images/winnie.png"/>
+<p class="health">health: <b> 60 </b></p>
+<div class="dice-container"><div class="dice"> 6 </div></div>
+</div>`;
+
+document.getElementById("monster").innerHTML = `<div class="character-card">
+<h4 class="name"> Squirrel </h4>
+<img class="avatar" src="images/squirrel.jpg"/>
+<p class="health">health: <b> 10 </b></p>
+<div class="dice-container"><div class="dice"> 4 </div></div>`;
