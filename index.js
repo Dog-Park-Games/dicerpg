b8ae7bfda121bcd75d89eacebf28cacb8994e732
martinwinther <mswinther@icloud.com>
2023-05-08 13:25:37 +0200

    Dive roll logic

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
-document.getElementById("hero").innerHTML = `<div class="character-card">
-<h4 class="name"> Winnie </h4>
-<img class="avatar" src="images/winnie.png"/>
-<p class="health">health: <b> 60 </b></p>
-<div class="dice-container"><div class="dice"> 6 </div></div>
-</div>`;
+const hero = {
+	id: "hero",
+	name: "Winnie",
+	avatar: "images/winnie.png",
+	health: "60",
+    diceCount: 3
+};
 
-document.getElementById("monster").innerHTML = `<div class="character-card">
-<h4 class="name"> Squirrel </h4>
-<img class="avatar" src="images/squirrel.jpg"/>
-<p class="health">health: <b> 10 </b></p>
-<div class="dice-container"><div class="dice"> 4 </div></div>`;
+const monster = {
+	id: "monster",
+	name: "Squirrel",
+	avatar: "images/squirrel.jpg",
+	health: "10",
+    diceCount: 1
+};
+
+function renderCharecter(data) {
+	const { id, name, avatar, health, diceCount } = data;
+    
+    const diceHtml = getDiceHtml(diceCount)
+
+	document.getElementById(id).innerHTML = `
+        <div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">
+                 ${diceHtml}
+             </div>
+        </div>
+        `;
+}
+
+function getDiceRollArray(diceCount) {
+    let newDiceRolls = []
+    for (let i = 0; i < diceCount; i ++) {        
+        newDiceRolls.push(Math.floor(Math.random() * 6 + 1))        
+    }
+    return newDiceRolls;
+
+}
+
+function getDiceHtml(diceCount) {
+    return getDiceRollArray(diceCount).map(function(num) {
+        return `<div class="dice">${num}</div>`
+        
+    }).join('')    
+}
+
+renderCharecter(hero);
+renderCharecter(monster);
