da737e32bd650a1780283fc58e2f262f87364193
martinwinther <mswinther@icloud.com>
2023-05-09 12:04:11 +0200

    Structure refactoring

---
+++ data.js
@@ -0,0 +1,15 @@
+const characterData = {
+    hero: {
+        name: "Winnie",
+        avatar: "images/winnie.png",
+        health: "60",
+        diceCount: 3    
+    },
+    monster: {
+        name: "Squirrel",
+        avatar: "images/squirrel.jpg",
+        health: "10",
+        diceCount: 1
+    }
+}
+export default characterData
